375db978a12c91099c22cfada9973330b9b62d01
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2023-03-29 21:23:18 +0900

    原型2

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-const Discord = require("discord.js");
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.GuildIntegrations,
+		GatewayIntentBits.GuildWebhooks,
+		GatewayIntentBits.GuildInvites,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildPresences,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessageTyping,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.DirectMessageReactions,
+		GatewayIntentBits.DirectMessageTyping,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildScheduledEvents,
+	],
+	partials: [
+		Partials.User,
+		Partials.Channel,
+		Partials.GuildMember,
+		Partials.Message,
+		Partials.Reaction,
+		Partials.GuildScheduledEvent,
+		Partials.ThreadMember,
+	],
+});
 const config = require("./config.json");
-const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("ready set go!");
 })
+
+
 
 client.login(config.token);
