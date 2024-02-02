d38960f46e5efa816090f2e3538b8e7d5091252f
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2024-02-02 17:01:27 +0900

    fix: :sparkles: コマンド読み込み処理をSlashCommandBuilder版に変更

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const { Client, Collection, Events, GatewayIntentBits, Partials } = require("discord.js");
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -28,57 +28,77 @@ const client = new Client({
 		Partials.ThreadMember,
 	],
 });
-// const fs = require("fs");
+const fs = require('node:fs');
+const path = require('node:path');
 const config = require("./config.json");
 
-// const commands = {}
-// const commandFiles = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
+// === コマンドファイルを読み込み ===
 
-// // 起動 and コマンド登録
+client.commands = new Collection();
 
-// for (const file of commandFiles) {
-// 	const command = require(`./commands/${file}`);
-// 	commands[command.data.name] = command;
-// }
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// === 初期化 ===
 
 client.once("ready", async () => {
-	// const data = []
-	// for (const commandName in commands) {
-	// 	data.push(commands[commandName].data)
-	// }
-	// await client.application.commands.set(data);
-console.log("im ready!")
+	console.log("im ready!")
 })
 
-client.on("interactionCreate", async interaction => {
-	if (!interaction.isCommand()) {
+/// === イベントハンドラー ===
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
 		return;
 	}
 
-	const command = commands[interaction.commandName];
 	try {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({
-		content: "コマンドの実行中にエラーが発生しました。",
-		ephemeral: true
-		})
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 	}
+
 });
+
+/// === Ctrl-Cで一瞬で殺せるように ===
 
 async function handle(signal) {
 	console.log(`Received ${signal}`)
 	await client.destroy()
 	process.exit()
-  }
-  
-  process.on('SIGINT', handle)
-  process.on('SIGTERM', handle)
-  process.on('uncaughtException', async (err) => {
-	await client.destroy()
-	console.error('uncaughtException:\n%o', err)
-	process.exit(1)
-  })
+	}
+
+	process.on('SIGINT', handle)
+	process.on('SIGTERM', handle)
+	process.on('uncaughtException', async (err) => {
+		await client.destroy()
+		console.error('uncaughtException:\n%o', err)
+		process.exit(1)
+	})
 
 client.login(config.token);
